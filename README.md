007312b3eb38940931c8357218d29fa458787dfc
phi <phi1618jp@gmail.com>
2015-08-06 18:26:12 +0900

    setup server

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # restify-sample
 restify-sample
+
+
+## Dev
+
+### setup
+
+db 起動
+
+```
+$ mongod -dbpath=db/
+```
+
+server 起動
+
+```
+$ node server.js
+```
+
+
+## Ref
+
+- http://hanamiju.hatenablog.com/entry/2015/03/05/225029
+- http://yutapon.hatenablog.com/entry/2014/04/29/124657
+- http://shimz.me/blog/node-js/1167
+
+
+### curl コマンド
+
+post
+
+curl http://0.0.0.0:8000/users -X POST -d "user[name]=phi" -d "user[age]=26"
+
+- http://d.hatena.ne.jp/thata/20100207/1265554365
--- server.js
+++ server.js
@@ -4,6 +4,51 @@ var server = restify.createServer();
 server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
+var mongoose = require('mongoose');
+var uristring = 'mongodb://localhost/';
+
+var db = mongoose.connect(uristring);
+var Schema = mongoose.Schema;
+
+// スキーマの定義
+var userSchema = new Schema({
+  score: Number,
+});
+
+// モデルを生成
+mongoose.model('user', userSchema);
+var User = mongoose.model('user');
+
+var users = {
+  index: function(req, res) {
+    User.find(function(arr, data){
+      // すべてのコレクションの情報を返す
+      res.send(data);
+    });
+    return 
+  },
+  show: function(req, res) {
+
+  },
+  create: function(req, res) {
+    var user = new User();
+    user.score = 10;
+    user.save(function(err, data) {
+      res.send({function:'Create', status: 'OK', id: data._id})
+    });
+  },
+  update: function(req, res) {
+
+  },
+  destroy: function(req, res) {
+
+  },
+};
+
+server.get('/users', users.index);
+server.get('/users:id', users.show);
+server.post('/users', users.create);
+
 server.get('/hoge', function(req, res, next) {
   res.send({message: "Hello, world!"});
 });
